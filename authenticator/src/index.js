8e0938aefc672b436c32e6d27717a2adc565d4ee
Nicolas Fraga <nfraga@uc.cl>
2022-11-17 18:21:04 -0300

    feat: users and tokens for first version ready

---
+++ authenticator/src/index.js
@@ -0,0 +1,17 @@
+const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
+const cors = require('@koa/cors')
+const { PORT } = process.env
+
+const app = new Koa()
+
+app.use(bodyParser())
+app.use(cors({
+  origin: '*'
+}))
+
+const { router } = require('./router')
+
+app.use(router.routes()).use(router.allowedMethods())
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}...`))
